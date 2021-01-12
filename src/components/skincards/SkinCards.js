acd3bf8979ad764bdb41db109fc12f308c5f4cf2
uday10056 <53361926+uday10056@users.noreply.github.com>
2021-01-12 19:39:59 +0530

    New Card

--- src/components/skincards/SkinCards.js
+++ src/components/skincards/SkinCards.js
@@ -6,7 +6,7 @@ const SkinCards = ({ list }) => {
     return (<>
         <Grid container alignContent="center" alignItems="center">
             {list.items.map((item, index) => {
-                return (<Grid item xs={12} sm={6} md={4} lg={4} key={item.id}>
+                return (<Grid item xs={12} sm={6} md={4} lg={3} key={item.id}>
                     <Grid container justify="center">
                         <Grid item>
                             <SkinCard title={item.title} publishedBy={item.publishedBy} image={item.image}></SkinCard>
--- src/components/skincards/skincard/SkinCard.js
+++ src/components/skincards/skincard/SkinCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
+import { Grid } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import PlayForWorkIcon from '@material-ui/icons/PlayForWork';
 
@@ -16,26 +17,42 @@ const useStyles = makeStyles({
         marginTop: 20,
         marginBottom: 20,
         maxWidth: 345,
+        color: "white",
     },
+    showClass: {
+        display: '',
+    },
+    hideClass: {
+        display: 'none',
+    },
+    titleColor:
+    {
+        color: "white",
+    }
 });
 
+
 export default function ImgMediaCard(props) {
+    const [showState, toggleShow] = useState(false);
     const classes = useStyles();
 
     return (
-        <Card className={classes.root}>
-            <CardActionArea >
-                <CardMedia
-                    component="img"
-                    alt="Contemplative Reptile"
-                    height="200"
-                    image={props.image}
-                    title="Contemplative Reptile"
+        <Card className={classes.root} style={{
+            backgroundImage: `url("https://cdn.discordapp.com/attachments/418467296395132928/798100013824999424/unknown.png")`, backgroundPepeat: "no-repeat",
+            backgroundPosition: "center center",
+            backgroundSize: "cover", height: "300px",
+            minWidth: 300,
+        }} onMouseEnter={() => {
+            toggleShow(true)
+        }} onMouseLeave={() => {
+            toggleShow(false)
+        }} >
+            <CardContent><Grid container>
+                <Grid item>
 
-                />
-                <CardContent>
 
-                    <CardHeader
+                    <CardHeader classes={{ subheader: classes.titleColor, title: classes.titleColor, avatar: classes.titleColor }} className={showState ? classes.showClass : classes.hideClass}
+
                         avatar={
                             <Avatar aria-label="recipe" className={classes.avatar}>
                                 R
@@ -43,7 +60,7 @@ export default function ImgMediaCard(props) {
                         }
                         action={
 
-                            <IconButton aria-label="settings">
+                            <IconButton color="inherit" aria-label=" settings">
                                 <PlayForWorkIcon fontSize="large" />
                             </IconButton>
 
@@ -54,9 +71,13 @@ export default function ImgMediaCard(props) {
 
                     />
 
-                </CardContent>
-            </CardActionArea>
 
-        </Card>
+
+                </Grid>
+            </Grid></CardContent>
+
+
+
+        </Card >
     );
 }
